de0a9e1a1e00a72f2f60a3d4b26bd0444c765f4f
lethanhtam190198 <tamle011998@gmail.com>
2022-06-13 08:29:25 +0700

    a

---
+++ ss1/student-management.sql
@@ -0,0 +1,13 @@
+create schema `student-management`;
+use `student-management`;
+create table `student-management`. `class`(
+`id` int not null,
+`name` varchar(45) null,
+primary key(`id`));
+
+create table `student-management`. `teacher`(
+`id` int not null,
+`name` varchar(45) null,
+`age` int null,
+`country` varchar(45) null,
+primary key(`id`));
