df18af40abff6dfedb74e71a20cabc2d4fbb90e6
lethanhtam190198 <tamle011998@gmail.com>
2022-06-17 07:44:06 +0700

    a

--- case_study/select_case_study.sql
+++ case_study/select_case_study.sql
@@ -2,11 +2,13 @@ use case_study;
 
 -- Cau 2:
 select * from nhan_vien 
-where ho_ten like 'H%' or ho_ten like 'T%' or ho_ten like 'K%' and char_length(ho_ten)<15;
+where ho_ten like 'H%' or ho_ten like 'T%' or ho_ten like 'K%' 
+and char_length(ho_ten)<15;
 
 -- Cau 3:
 select * from khach_hang 
-where ((YEAR(CURDATE()) - YEAR(ngay_sinh)) between 18 and 50) and (dia_chi like "%Đà nẵng" or dia_chi like "%Quảng trị");
+where ((YEAR(CURDATE()) - YEAR(ngay_sinh)) between 18 and 50) 
+and (dia_chi like "%Đà nẵng" or dia_chi like "%Quảng trị");
 
 -- Cau 4:
 select hd.ma_khach_hang, kh.ho_ten , count(hd.ma_khach_hang) 
@@ -21,12 +23,78 @@ order by so_lan_thue ;
 select 
 kh.ma_khach_hang,
 kh.ho_ten,
-loai_khach.ten_loai_khach,
-hop_dong.ma_hop_dong,
-dich_vu.ten_dich_vu,
-hop_dong.ngay_lam_hop_dong,
-hop_dong.ngay_ket_thuc,
-sum(dich_vu.chi_phi_thue+(hop_dong_chi_tiet.so_luong *dich_vu_di_kem.gia)) as tong_tien
-from khach_hang kh, hop_dong hd, loai_khach lk
-join kh on kh.ma_loai_khach=loai_khach.ma_loai_khach
-right join hop_dong hd on hd.ma_khach_hang= kh.ma_khach_hang
+lk.ten_loai_khach,
+hd.ma_hop_dong,
+dv.ten_dich_vu,
+hd.ngay_lam_hop_dong,
+hd.ngay_ket_thuc,
+ifnull(dv.chi_phi_thue,0)+ ifnull(hdct.so_luong * dvdk.gia,0)
+as tong_tien
+from khach_hang kh
+left join loai_khach lk on lk.ma_loai_khach=kh.ma_loai_khach
+left join hop_dong hd on hd.ma_khach_hang= kh.ma_khach_hang
+left join dich_vu dv on hd.ma_dich_vu= dv.ma_dich_vu
+left join hop_dong_chi_tiet hdct on hd.ma_hop_dong=hdct.ma_hop_dong
+left join dich_vu_di_kem dvdk on hdct.ma_dich_vu_di_kem=dvdk.ma_dich_vu_di_kem
+group by kh.ma_khach_hang;
+
+-- 6.	Hiển thị ma_dich_vu, ten_dich_vu, dien_tich, 
+-- chi_phi_thue, ten_loai_dich_vu của tất cả các loại dịch vụ 
+-- chưa từng được khách hàng thực hiện đặt từ quý 1 của năm 2021 
+-- (Quý 1 là tháng 1, 2, 3).
+
+select 
+dv.ma_dich_vu,
+dv.ten_dich_vu,
+dv.dien_tich,
+dv.chi_phi_thue,
+ldv.ten_loai_dich_vu
+from hop_dong hd
+join dich_vu dv on hd.ma_dich_vu= dv.ma_dich_vu 
+join loai_dich_vu ldv on dv.ma_loai_dich_vu= ldv.ma_loai_dich_vu
+group by dv.ma_dich_vu
+having dv.ma_dich_vu not in
+( select dv.ma_dich_vu from hop_dong hd
+join dich_vu dv on hd.ma_dich_vu= dv.ma_dich_vu
+join loai_dich_vu ldv on dv.ma_loai_dich_vu= ldv.ma_loai_dich_vu
+where month(hd.ngay_lam_hop_dong) between 1 and 3 and year(hd.ngay_lam_hop_dong)='2021' );
+
+-- 7.	Hiển thị thông tin ma_dich_vu, ten_dich_vu, dien_tich, 
+-- so_nguoi_toi_da, chi_phi_thue, ten_loai_dich_vu của tất cả các 
+-- loại dịch vụ đã từng được khách hàng đặt phòng trong năm 2020 
+-- nhưng chưa từng được khách hàng đặt phòng trong năm 2021.
+
+select 
+dv.ma_dich_vu,
+dv.ten_dich_vu,
+dv.dien_tich,
+dv.so_nguoi_toi_da,
+dv.chi_phi_thue,
+hd.ngay_lam_hop_dong,
+ldv.ten_loai_dich_vu
+from hop_dong hd
+join dich_vu dv on hd.ma_dich_vu=dv.ma_dich_vu
+join loai_dich_vu ldv on dv.ma_loai_dich_vu= ldv.ma_loai_dich_vu
+where year(hd.ngay_lam_hop_dong)='2020'
+group by dv.ma_dich_vu
+having dv.ma_dich_vu not in(
+select dv.ma_dich_vu from hop_dong hd
+join dich_vu dv on hd.ma_dich_vu=dv.ma_dich_vu
+join loai_dich_vu ldv on dv.ma_loai_dich_vu= ldv.ma_loai_dich_vu
+where year(hd.ngay_lam_hop_dong)='2021');
+
+
+-- 8.	Hiển thị thông tin ho_ten khách hàng có trong hệ thống, 
+-- với yêu cầu ho_ten không trùng nhau.
+-- Học viên sử dụng theo 3 cách khác nhau để thực hiện yêu cầu trên.
+
+SELECT DISTINCT ho_ten from khach_hang;
+
+select khach_hang.ho_ten from khach_hang
+group by khach_hang.ho_ten;
+
+select khach_hang.ho_ten from khach_hang
+union
+select khach_hang.ho_ten from khach_hang;
+
+
